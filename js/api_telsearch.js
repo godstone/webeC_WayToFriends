06db18550d68519cfe556a56906af42cdcb0cf81
Tilip Thiriuchchelvam <prathap.thiruchchelvam@students.fhnw.ch>
2016-06-13 16:56:06 +0200

    - Result view adjusted - JQuery version 3 added

--- js/api_telsearch.js
+++ js/api_telsearch.js
@@ -42,10 +42,10 @@ function showAdresses(searchKey, element){
                 var saveMyDiv = document.getElementById('searchResult').innerHTML;
                 document.getElementById('searchResult').innerHTML = saveMyDiv + '<fieldset>' +
                     '<div class="searchedContact">' +
-                    '<div class="star"><i class="fa fa-star" aria-hidden="false"></i></div>' +
-                    '<div class="contactContent"><span class="contactName">' + getValueFromEntry(entries[i], "name") + '</span><br/>' +
+                    '<div class="star"><i class="fa fa-star-o" aria-hidden="false"></i></div>' +
+                    '<div class="contactContent"><span class="contactName">' + getValueFromEntry(entries[i], "name") + '</span> ' +
                     '<span class="contactFirstname">' + getValueFromEntry(entries[i], "firstname") + '</span><br/>' +
-                    '<span class="contactStreet">' + getValueFromEntry(entries[i], "street") + ' ' + getValueFromEntry(entries[i], "streetno") + '</span><br/>' +
+                    '<span class="contactStreet">' + getValueFromEntry(entries[i], "street") + ' ' + getValueFromEntry(entries[i], "streetno") + '</span>, ' +
                     '<span class="contactZip">' + getValueFromEntry(entries[i], "zip") + ' ' + getValueFromEntry(entries[i], "city") + '</span><br/>' +
                     '<span class="contactPhone">' + getValueFromEntry(entries[i], "phone") + '</span><br/>' +
                     '<span class="contactId">' + getValueFromEntry(entries[i], "id") + '</span>'
@@ -66,3 +66,4 @@ function showAdresses(searchKey, element){
         }
     }
 }
+
